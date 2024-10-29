9a6839faa52f555d455c070ac963e68e19275aae
Lucas Chavez <luck9217@gmail.com>
2024-10-29 18:39:37 +1100

    fix:caching year footer

--- scripts/year.js
+++ scripts/year.js
@@ -1,9 +1,3 @@
 document.getElementById(
   "footer-text"
 ).innerHTML = `@${new Date().getFullYear()} Bryan's Café`;
-
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-function toggleMenu() {
-  const navLinks = document.querySelector(".nav-links");
-  navLinks.classList.toggle("active");
-}
