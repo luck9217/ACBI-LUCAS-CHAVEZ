d459729ce33c09d9ee99dd8bce5221845e793cac
Lucas Chavez <luck9217@gmail.com>
2024-10-31 17:42:10 +1100

    feat:adding privacy page

--- scripts/year.js
+++ scripts/year.js
@@ -1,3 +1,3 @@
 document.getElementById(
   "footer-text"
-).innerHTML = `©${new Date().getFullYear()} Bryan's Café`;
+).innerHTML = `©${new Date().getFullYear()} Bryan's Café | <a href="privacy.html" target="_blank">Privacy Policy</a>`;
